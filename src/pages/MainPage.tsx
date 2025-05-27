96a9b23875bd0b816e0ef44f477af07758478673
Steel64 <updown1.jp@gmail.com>
2025-05-26 19:25:54 -0500

    conexion funcional con baseDatos(resp_post)

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,46 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Formulario from "../components/Formulario"
 import ListaTODOS, { type ToDo } from "../components/ListaTODOS"
 import Navegacion, { Pagina } from "../components/Navegacion"
 import Titulo from "../components/Titulo"
 
 const URL = "https://script.google.com/macros/s/AKfycbxR06kwYzBmVIy9NoLCq1ddnLj4PIT9uGvPNiK_I5aAob7qrYUs-Q7XPfLab3Lk1ZD9KQ/exec"
-/*
-const httpObtenerTODOS = () => {
-    //realiza la peticion web al servidor y recibe una promesa de respuesta
-    const resp = fetch(URL)
-    //colocar la funcion cuando se recibe la respuesta
-    //resp - response
-    resp.then((resp)=>{
-        //json - promesa
-        const resp2 =  resp.json()
-        //data obtendida
-        resp2.then((data)=>{
-            console.log(data)
-        })
-    })
-    //evaluar si hay errores
-    resp.catch((error)=> {
-        console.error(error)
-    })
-}
-*/
-//forma mas simple
-const httpObtenerTODOSAsyncAwait = async () => {
-    try{
-        //await permite que se ejecute como asincrona
-        //fetch funciona como sincrono y await lo convierte asincrono
-        const resp = await fetch(URL)
-        const data = await resp.json()
-        console.log(data)
-    }catch(error){
-        console.error(error)
-    }
-}
-//httpObtenerTODOS()
-httpObtenerTODOSAsyncAwait()
 
 const MainPage = () => {
+    /*
     const listaPersistenteStr = sessionStorage.getItem("TODOS")
     let listaPersistente : ToDo[]
     if (listaPersistenteStr == null){
@@ -48,14 +15,42 @@ const MainPage = () => {
     } else {
         listaPersistente = JSON.parse(listaPersistenteStr)
     }
-    const [listaTODOS,setlistaTODOS] = useState<ToDo[]>(listaPersistente)
+    */
+    const [listaTODOS,setlistaTODOS] = useState<ToDo[]>([])
+    //funcion asicrona
+    const httpObtenerTODOSAsyncAwait = async () => {
+        try{
+            const resp = await fetch(URL)
+            const data = await resp.json()
+            console.log(data)
+            setlistaTODOS(data)
+        }catch(error){
+            console.error(error)
+        }
+    }
+    //la peticion en fetch como post, body debe enviarse como string(formato JSON  del sevidor)
+    const httpGuardarTODO = async (todo: ToDo) => {
+        const resp = await fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(todo)
+        })
+        const data = await resp.json()
+    }
+
+    useEffect(()=> {
+        httpObtenerTODOSAsyncAwait()
+    }, [])
+
     const agregarTODOS = ( texto: string) => {
         console.log("aca")
+        httpGuardarTODO({
+            id: listaTODOS.length + 1,
+            descripcion: texto
+        })
         listaTODOS.push({
             id: listaTODOS.length + 1,
             descripcion: texto
         })
-        sessionStorage.setItem("TODOS",JSON.stringify(listaTODOS))
         setlistaTODOS([...listaTODOS])
     }
 
